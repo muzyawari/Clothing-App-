34b498ef66339d7062a8042b18103dd9a0bc3168
Muz <muzamilyawari@gmail.com>
2022-06-02 17:01:39 +1000

    installed react router

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,113 +1,16 @@
-import "../css/Home.styles.css";
+import HomeGrid from "./HomeGrid";
 
 export default function Home() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
     <div className="flex justify-center items-center">
       <div className="2xl:mx-auto 2xl:container py-12 sm:px-6 xl:px-20 2xl:px-0 w-full px-6">
         <div className="flex flex-col jusitfy-center items-center space-y-10">
           <div className="flex flex-col justify-center items-center ">
-            <h1 className="text-2xl xl:text-3xl font-semibold leading-7 xl:leading-9 text-gray-700 dark:text-white">
+            <h1 className="text-2xl xl:text-3xl leading-7 xl:leading-9 text-gray-700 dark:text-white">
               Start Shopping
             </h1>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-4 md:gap-x-8 w-full">
-            <div className="relative group flex justify-center items-center h-full w-full">
-              <img
-                className="home__image"
-                src="https://i.ibb.co/ThPFmzv/omid-armin-m-VSb6-PFk-VXw-unsplash-1-1.png"
-                alt="girl-image"
-              />
-              <button className="home__button">Womens</button>
-            </div>
-
-            <div className="flex flex-col space-y-4 md:space-y-8 mt-4 md:mt-0">
-              <div className="relative group flex justify-center items-center h-full w-full">
-                <img
-                  className="home__image"
-                  src="https://i.ibb.co/SXZvYHs/irene-kredenets-DDqx-X0-7v-KE-unsplash-1.png"
-                  alt="shoe-image"
-                />
-                <button className="home__button">Mens</button>
-                <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
-              </div>
-              <div className="relative group flex justify-center items-center h-full w-full">
-                <img
-                  className="home__image"
-                  src="https://i.ibb.co/Hd1pVxW/louis-mornaud-Ju-6-TPKXd-Bs-unsplash-1-2.png"
-                  alt="watch-image"
-                />
-                <button className="home__button">Watches</button>
-                <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
-              </div>
-            </div>
-
-            <div className="relative group justify-center items-center h-full w-full hidden lg:flex">
-              <img
-                className="home__image"
-                src="https://i.ibb.co/PTtRBLL/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png"
-                alt="girl-image"
-              />
-              <button className="home__button">Accessories</button>
-              <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
-            </div>
-
-            <div className="relative group flex justify-center items-center h-full w-full mt-4 md:hidden md:mt-8 lg:hidden">
-              <img
-                className="object-center object-cover h-full w-full hidden md:block"
-                src="https://i.ibb.co/6FjW19n/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2.png"
-                alt="girl-image"
-              />
-              <img
-                className="object-center object-cover h-full w-full md:hidden"
-                src="https://i.ibb.co/sQgHwHn/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png"
-                alt="olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2"
-              />
-              <button className="home__button">Accessories</button>
-              <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
-            </div>
-          </div>
-          <div className="relative group hidden md:flex justify-center items-center h-full w-full mt-4 md:mt-8 lg:hidden">
-            <img
-              className="object-center object-cover h-full w-full hidden md:block"
-              src="https://i.ibb.co/6FjW19n/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2.png"
-              alt="girl-image"
-            />
-            <img
-              className="object-center object-cover h-full w-full sm:hidden"
-              src="https://i.ibb.co/sQgHwHn/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png"
-              alt="olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2"
-            />
-            <button className="home__button">Accessories</button>
-            <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
-          </div>
+          <HomeGrid />
         </div>
       </div>
     </div>
--- src/components/HomeGrid.js
+++ src/components/HomeGrid.js
@@ -1,5 +1,78 @@
-import React from "react";
+import "../css/Home.styles.css";
 
 export default function HomeGrid() {
-  return <div>HomeGrid</div>;
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-4 md:gap-x-8 w-full">
+        <div className="relative group flex justify-center items-center h-full w-full">
+          <img
+            className="home__image"
+            src="https://i.ibb.co/ThPFmzv/omid-armin-m-VSb6-PFk-VXw-unsplash-1-1.png"
+            alt="girl-image"
+          />
+          <button className="home__button">Womens</button>
+        </div>
+
+        <div className="flex flex-col space-y-4 md:space-y-8 mt-4 md:mt-0">
+          <div className="relative group flex justify-center items-center h-full w-full">
+            <img
+              className="home__image"
+              src="https://i.ibb.co/SXZvYHs/irene-kredenets-DDqx-X0-7v-KE-unsplash-1.png"
+              alt="shoe-image"
+            />
+            <button className="home__button">Mens</button>
+            <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
+          </div>
+          <div className="relative group flex justify-center items-center h-full w-full">
+            <img
+              className="home__image"
+              src="https://i.ibb.co/Hd1pVxW/louis-mornaud-Ju-6-TPKXd-Bs-unsplash-1-2.png"
+              alt="watch-image"
+            />
+            <button className="home__button">Watches</button>
+            <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
+          </div>
+        </div>
+
+        <div className="relative group justify-center items-center h-full w-full hidden lg:flex">
+          <img
+            className="home__image"
+            src="https://i.ibb.co/PTtRBLL/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png"
+            alt="girl-image"
+          />
+          <button className="home__button">Accessories</button>
+          <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
+        </div>
+
+        <div className="relative group flex justify-center items-center h-full w-full mt-4 md:hidden md:mt-8 lg:hidden">
+          <img
+            className="object-center object-cover h-full w-full hidden md:block"
+            src="https://i.ibb.co/6FjW19n/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2.png"
+            alt="girl-image"
+          />
+          <img
+            className="object-center object-cover h-full w-full md:hidden"
+            src="https://i.ibb.co/sQgHwHn/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png"
+            alt="olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2"
+          />
+          <button className="home__button">Accessories</button>
+          <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
+        </div>
+      </div>
+      <div className="relative group hidden md:flex justify-center items-center h-full w-full mt-4 md:mt-8 lg:hidden">
+        <img
+          className="object-center object-cover h-full w-full hidden md:block"
+          src="https://i.ibb.co/6FjW19n/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2.png"
+          alt="girl-image"
+        />
+        <img
+          className="object-center object-cover h-full w-full sm:hidden"
+          src="https://i.ibb.co/sQgHwHn/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png"
+          alt="olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2"
+        />
+        <button className="home__button">Accessories</button>
+        <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
+      </div>
+    </>
+  );
 }
